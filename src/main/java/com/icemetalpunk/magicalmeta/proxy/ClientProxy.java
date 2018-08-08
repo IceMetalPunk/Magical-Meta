45b550174d6be65d7a60b370e4de6b6253d7b8fc
IceMetalPunk <icemetalpunk@gmail.com>
2018-08-07 22:13:36 -0400

    Fixed proxy inheritance calls

--- src/main/java/com/icemetalpunk/magicalmeta/proxy/ClientProxy.java
+++ src/main/java/com/icemetalpunk/magicalmeta/proxy/ClientProxy.java
@@ -14,16 +14,16 @@ public class ClientProxy extends CommonProxy {
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
-
+		super.preInit(event);
 	}
 
 	@Override
 	public void init(FMLInitializationEvent event) {
-
+		super.init(event);
 	}
 
 	@Override
 	public void postInit(FMLPostInitializationEvent event) {
-
+		super.postInit(event);
 	}
 }
